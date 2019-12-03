48f6fe87a25905cac1c1ca00fc138c43577b3047
Jelte Wiersma <jelte_wiersma@hotmail.com>
2019-12-03 22:06:31 +0100

    group muscles if needed

--- index.js
+++ index.js
@@ -10,23 +10,75 @@ const puppeteer = require('puppeteer');
   }  
 
   const bodyPartsAndMuscles = await page.evaluate(() => {
+    const groupMuscles = (muscle) => {
+      let name = '';
+      switch (muscle) {
+        case 'Anterior':
+          name = 'Deltoid - Anterior'
+        break;
+        case 'Lateral':
+          name = 'Deltoid - Lateral'
+        break;
+        case 'Posterior':
+          name = 'Deltoid - Posterior'
+        break;
+        case 'Flexors':
+          name = 'Wrist - Flexors'
+        break;
+        case 'Extensors':
+            name = 'Wrist - Extensors'
+        break;
+        case 'Upper':
+            name = 'Trapezius - Upper'
+        break;
+        case 'Middle':
+            name = 'Trapezius - Middle'
+        break;
+        case 'Lower':
+            name = 'Trapezius - Lower'
+        break;
+        case 'Sternal':
+            name = 'Pectoralis Major - Sternal'
+        break;
+        case 'Clavicular':
+            name = 'Pectoralis Major - Clavicular'
+        break;
+        default:
+          name = muscle;
+        break;
+      }
+      return name;
+    }
     const bodyPartsElements = document.querySelectorAll('.col-sm-6 > ul > li > a');
 
-    // const musclesElements = document.querySelectorAll('.col-sm-6 > ul > li > ul');
+    const musclesElements = document.querySelectorAll('.col-sm-6 > ul > li');
     
     const data = [];
     for(let i = 0; i < bodyPartsElements.length; i++) {
-        data.push({"name": bodyPartsElements[i].innerText});
+      // muscles
+      let muscleArray = musclesElements[i].innerText.split('\n');
+      // remove first entry as it repeats the name itself
+      muscleArray.shift();
+      let muscleObjects = [];
+      muscleArray.forEach(element => {
+        // non-clickable areas
+        if (element !== 'Deltoid' || 'Wrist' || 'Trapezius' || 'Pectoralis Major') {
+          muscleObjects.push({"name": groupMuscles(element)});
+        }
+      });
+
+      // output
+      data.push(
+        {
+          "name": bodyPartsElements[i].innerText, 
+          "muscles": muscleObjects
+        });
     }
     return data;
   });
 
   finalData.bodyparts.push(bodyPartsAndMuscles);
 
-
-
-
-
-//   console.log(JSON.stringify(finalData));
+  console.log(JSON.stringify(finalData));
   await browser.close();
 })();
